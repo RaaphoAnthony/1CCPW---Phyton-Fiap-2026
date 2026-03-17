6c7144f0f3299a3b3a324f96d56ebcf98d597dc5
raphaellocaffettani <contato.raapha@gmail.com>
2026-03-16 22:51:06 -0300

    aula 2

---
+++ aula02-variaveis-operadores/Desafio.py
@@ -0,0 +1,16 @@
+nome = input("Digite seu nome: ") # str
+print(f'Boas vindas , {nome}')
+
+dia = input("Digite o dia do seu aniversário: ")
+mes = input("Digite o mes do seu aniversário: ")
+ano = input("Digite o ano do seu aniversário: ")
+
+print(f'Seu aniversário é  , {dia}/{mes}/{ano}')
+
+numero1 = int(input("Digite o primeiro numero: "))
+numero2 = int(input("Digite o segundo numero: "))
+
+print(f'A soma é {numero1+numero2}')
+
+
+
---
+++ aula02-variaveis-operadores/aula02a-variaveis.py
@@ -0,0 +1,30 @@
+print("Ola mundo")
+
+print(7 + 4)
+print("7 + 4")
+print("7" + "4") # CONCATENA STRINGS
+
+# Comentários de 1 linha no python
+
+'''
+   Comentarios de 
+   multiplas
+   linhas
+'''
+
+# VARIAVEIS
+
+nome = "Alexandre" # str
+idade = 26 # int
+peso = 70.2  # float
+
+print(nome, idade, peso )
+
+
+#INPUT - SIMULAR UM FORMULARIO N CMO
+nome = input("Digite seu nome: ") # str
+idade = int(input("Digite sua idade: "))
+peso = float(input("Digite seu peso: "))
+
+print(nome, idade, peso)
+print)(f'Oii, {nome}')
